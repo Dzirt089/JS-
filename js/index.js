a87421bad4ad5fee2ae24c8b2e629d668d4f9f3c
Ilya Emets <suoza089@gmail.com>
2021-09-30 22:18:06 +0300

    Add modal window

--- js/index.js
+++ js/index.js
@@ -90,4 +90,38 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
     setClock('.timer', deadline);
+
+    // Modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close]');
+
+    function modalClose() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+    
+    modalCloseBtn.addEventListener('click', modalClose);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+        modalClose();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === "Escape" && modal.classList.contains('show')) {
+            modalClose();
+        }
+    });
 });
