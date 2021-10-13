c8aa650ef99aa3e344dbd576451f9aafeec0c6ed
Ilya Emets <suoza089@gmail.com>
2021-10-13 21:54:34 +0300

    Добавил axios

--- js/script.js
+++ js/script.js
@@ -178,36 +178,58 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    new MenuCard(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        '.menu .container',
-        'menu__item'
-    ).render();
+    const getResource = async (url) => {
+        const res = await fetch(url);
 
-    new MenuCard(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        14,
-        '.menu .container',
-        'menu__item'
-    ).render();
+        if(!res.ok){
+            throw new Error (`Could not fetch ${url}, status: ${res.status}`);
+        }
 
-    new MenuCard(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        21,
-        '.menu .container',
-        'menu__item'
-    ).render();
+        return await res.json();
+    };
 
+
+    axios.get('http://localhost:3000/menu')
+    .then(data => {
+        data.data.forEach(({img, altimg, title, descr, price}) => {
+            new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+        });
+    });
+
+    
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => {
+    //         data.forEach(({img, altimg, title, descr, price}) => {
+    //             new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+    //         });
+    //     });
+
+        //Диамически созданные карточки, без шаблонизации классом
+
+
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => createCard(data));
+
+    
+    // function createCard(data) {
+    //     data.forEach(({img, altimg, title, descr, price}) => {
+    //         const element = document.createElement('div');
+    //         element.classList.add('menu__item');
+
+    //         element.innerHTML = `
+    //             <img src=${img} alt=${altimg}>
+    //                 <h3 class="menu__item-subtitle">${title}</h3>
+    //                 <div class="menu__item-descr">${descr}</div>
+    //                 <div class="menu__item-divider"></div>
+    //                 <div class="menu__item-price">
+    //                     <div class="menu__item-cost">Цена:</div>
+    //                     <div class="menu__item-total"><span>${price}</span> грн/день</div>
+    //                 </div>
+    //         `;
+
+    //         document.querySelector('.menu .container').append(element);
+    //     });
+    // }  Конец примера
     // Forms
 
     const forms = document.querySelectorAll('form');
@@ -220,12 +242,24 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Теперь мы должны взять все наши формы и подвязать их к нашей фукции postData()
     forms.forEach(item => {
-        postData(item);
+        bindPostData(item);
     });
 
-// Сейчас на каждую форму у нас подвязана функция postData, которая будет обработчиком события при отправке (нажатии кнопки отправить ("submit"))
+    const postData = async (url, data) => {
+        const res = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: data
+        });
 
-    function postData(form) {     //Берём все наши формы, чтобы не собирать по отдельности в ручную.
+        return await res.json();
+    };
+
+    // Сейчас на каждую форму у нас подвязана функция postData, которая будет обработчиком события при отправке (нажатии кнопки отправить ("submit"))
+
+    function bindPostData(form) {     //Берём все наши формы, чтобы не собирать по отдельности в ручную.
         form.addEventListener('submit' , (e) => {
             e.preventDefault(); //отменяем стандартное поведение браузера, а именно перезагрузка стр при отправке формы
             const statusMessage = document.createElement('img');
@@ -234,25 +268,15 @@ window.addEventListener('DOMContentLoaded', () => {
                 display: block;
                 margin: 0 auto;
             `;
-            //form.append(statusMessage);// К форме мы добавляем наше сообщение
+    //form.append(statusMessage);// К форме мы добавляем наше сообщение
             form.insertAdjacentElement('afterend', statusMessage); // этот метод позволяет добавить любой элемент в любую точку нашей верстки. И не ломает наш флекс
-            // с помощью FormData собираем все формы. И при помощи fetch-а мы просто берем и отправляем эти данные
+    // с помощью FormData собираем все формы. И при помощи fetch-а мы просто берем и отправляем эти данные
             const formData = new FormData(form);
-            //в верстке написаны инпуты, и всегда должен присутствовать атрибут name. Иначе удет куча ошибок и FormData не сможет отработать правильно.
+    //в верстке написаны инпуты, и всегда должен присутствовать атрибут name. Иначе удет куча ошибок и FormData не сможет отработать правильно.
 
-            const object = {};
-            formData.forEach(function(value, key) {
-                object[key] = value;
-            });
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-            fetch('server.php', {
-                method: "POST",
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(object)
-            })
-            .then(data => data.text())
+            postData('http://localhost:3000/requests', json)
             .then(data => {
                 console.log(data);
                 showThanksModal(message.success);
@@ -289,16 +313,15 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 4000);
     }
     //Идёт простая инструкция, показать блок, создать блок, наполнить его контентом, и т.д.
-    fetch('http://localhost:3000/menu')
-        .then(data => data.json())
-        .then(res => console.log(res));
+
     
-// Во-первых, вам нужно проверить, установлен глобально или нет json-server. или вы можете установить его глобально
+    // Во-первых, вам нужно проверить, установлен глобально или нет json-server. или вы можете установить его глобально
 
-// npm install -g json-server
-// Если вы устанавливаете его локально в своем проекте, используйте npx для его запуска
+    // npm install -g json-server
+    // Если вы устанавливаете его локально в своем проекте, используйте npx для его запуска
 
-// npx json-server --watch db.json
+    // npx json-server --watch db.json
+
 
 
 });
